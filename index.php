600caae1463f34cd5e7cd9cc4ea5ed8da6e178e6
Nipun Prasad Gunawardhana <nipungunawardhana71@gmail.com>
2025-04-28 18:44:21 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>sample php cms</title>
+    <link rel="stylesheet" href="css/boostrap.css" media="screen" title="no title" charset="utf-8" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<body>
+    <h2>login form</h2>
+    <form class="" action="process_login.php" method="post">
+        <div class="form-group">
+            <label for="username">Username:</label>
+            <input type="text" class="form-control" id="username" name="username" required>
+        </div>
+        <div class="form-group">
+            <label for="Password">Password:</label>
+            <input type="password" class="form-control" id="password" name="password" required>
+        </div>
+        <div class="form-group">
+            
+            <input type="submit" class="btn btn-primary" value="Login">
+        </div>
+
+</body>
+</html>
